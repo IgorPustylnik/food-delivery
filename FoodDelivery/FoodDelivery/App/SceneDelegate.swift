6cc3a298be722bb1f2486c723e636452ab6d4f7b
igorpustylnik <darthveider36@gmail.com>
2024-11-30 22:51:37 +0300

    Refactor file structure, add SwiftGen and SwiftLint

--- FoodDelivery/FoodDelivery/App/SceneDelegate.swift
+++ FoodDelivery/FoodDelivery/App/SceneDelegate.swift
@@ -11,14 +11,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
         window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
-        
+
         let navigationController = UINavigationController(rootViewController: ViewController())
-        
+
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
@@ -38,6 +41,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidEnterBackground(_ scene: UIScene) {
     }
 
-
 }
-
