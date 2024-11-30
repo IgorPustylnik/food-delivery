a76f44643a593efc60f9a546e7a9aed2af4378fa
igorpustylnik <darthveider36@gmail.com>
2024-11-30 18:07:21 +0300

    Create project

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Игорь Пустыльник on 30.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
