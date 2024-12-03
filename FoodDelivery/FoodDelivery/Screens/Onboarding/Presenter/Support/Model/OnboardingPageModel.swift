29bf72899084aadbcf46f996e681f7953487feaa
IgorPustylnik <darthveider36@gmail.com>
2024-12-03 13:41:14 +0300

    Add Onboarding pages model

---
+++ FoodDelivery/FoodDelivery/Screens/Onboarding/Presenter/Support/Model/OnboardingPageModel.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingPageModel.swift
+//  FoodDelivery
+//
+//  Created by Игорь Пустыльник on 03.12.2024.
+//
+
+import UIKit
+
+struct OnboardingPageModel {
+    let title: String
+    let description: String
+    let image: UIImage
+}
---
+++ FoodDelivery/FoodDelivery/Screens/Onboarding/Presenter/Support/Model/OnboardingPresenterModel.swift
@@ -0,0 +1,53 @@
+//
+//  OnboardingPresenterModel.swift
+//  FoodDelivery
+//
+//  Created by Игорь Пустыльник on 03.12.2024.
+//
+
+import Foundation
+
+enum OnboardingPresenterModel {
+
+    enum Page {
+        case page1, page2, page3, page4
+
+        var pageModel: OnboardingPageModel {
+            switch self {
+            case .page1:
+                return OnboardingPageModel(
+                    title: L10n.Onboarding.Page1.title,
+                    description: L10n.Onboarding.Page1.description,
+                    image: .Onboarding.page1
+                )
+            case .page2:
+                return OnboardingPageModel(
+                    title: L10n.Onboarding.Page2.title,
+                    description: L10n.Onboarding.Page2.description,
+                    image: .Onboarding.page2
+                )
+            case .page3:
+                return OnboardingPageModel(
+                    title: L10n.Onboarding.Page3.title,
+                    description: L10n.Onboarding.Page3.description,
+                    image: .Onboarding.page3)
+            case .page4:
+                return OnboardingPageModel(
+                    title: L10n.Onboarding.Page4.title,
+                    description: L10n.Onboarding.Page4.description,
+                    image: .Onboarding.page4
+                )
+            }
+        }
+
+        var buttonTitle: String {
+            switch self {
+            case .page4:
+                return L10n.Onboarding.Button.last
+            default:
+                return L10n.Onboarding.Button.next
+            }
+        }
+    }
+
+}
