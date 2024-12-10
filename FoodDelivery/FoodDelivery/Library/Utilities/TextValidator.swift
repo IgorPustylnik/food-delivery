18c80726e6be2c395575a0b8fbd1c061aaa4509f
IgorPustylnik <darthveider36@gmail.com>
2024-12-10 10:57:58 +0300

    Decompose TextValidator main method

--- FoodDelivery/FoodDelivery/Library/Utilities/TextValidator.swift
+++ FoodDelivery/FoodDelivery/Library/Utilities/TextValidator.swift
@@ -14,34 +14,67 @@ enum TextValidator {
     case username
     case email
     case password
+    case notEmpty
+    case match(password: String)
 
     func validate(_ text: String?) -> ValidationResult {
         guard let text else {
-            return (false, "Empty")
+            return (false, "Validation failed")
         }
+
         switch self {
         case .username:
-            guard text.count > 2 else {
-                return (false, L10n.InvalidInput.Username.short)
-            }
-            guard text.count < 50 else {
-                return (false, L10n.InvalidInput.Username.long)
-            }
-            return (true, nil)
+            return validateUsername(text)
         case .email:
-            let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-            guard NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: text) else {
-                return (false, L10n.InvalidInput.email)
-            }
-            return (true, nil)
+            return validateEmail(text)
         case .password:
-            guard text.count > 8 else {
-                return (false, L10n.InvalidInput.Password.long)
-            }
-            guard text.count < 50 else {
-                return (false, L10n.InvalidInput.Password.long)
-            }
-            return (true, nil)
+            return validatePassword(text)
+        case .notEmpty:
+            return validateRequired(text)
+        case .match(let password):
+            return validateMatchPassword(text, password: password)
         }
+    }
+
+    private func validateUsername(_ text: String) -> ValidationResult {
+        if text.count < 2 {
+            return (false, L10n.InvalidInput.Username.short)
+        }
+        if text.count > 50 {
+            return (false, L10n.InvalidInput.Username.long)
+        }
+        return (true, nil)
+    }
+
+    private func validateEmail(_ text: String) -> ValidationResult {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        if !NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: text) {
+            return (false, L10n.InvalidInput.email)
+        }
+        return (true, nil)
+    }
+
+    private func validatePassword(_ text: String) -> ValidationResult {
+        if text.count < 8 {
+            return (false, L10n.InvalidInput.Password.short)
+        }
+        if text.count > 50 {
+            return (false, L10n.InvalidInput.Password.long)
+        }
+        return (true, nil)
+    }
+
+    private func validateRequired(_ text: String) -> ValidationResult {
+        if text.isEmpty {
+            return (false, L10n.InvalidInput.Required.empty)
+        }
+        return (true, nil)
+    }
+
+    private func validateMatchPassword(_ text: String, password: String) -> ValidationResult {
+        if text != password {
+            return (false, L10n.InvalidInput.Password.mismatch)
+        }
+        return (true, nil)
     }
 }
